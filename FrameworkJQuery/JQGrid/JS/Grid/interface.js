70128189d7303292ba78b89f99df06b2492fb405
bertuc_k <kenni.bertucat@epitech.eu>
2011-11-17 10:35:51 +0100

    Mise a jour

---
+++ FrameworkJQuery/JQGrid/JS/Grid/interface.js
@@ -0,0 +1,16 @@
+// Toute les option sont dispo sut : http://www.trirand.com/jqgridwiki/doku.php?id=wiki:options
+
+var gridName = "Nom de la grid";
+
+var source = [
+                {name:"Number", type:"int"},
+                {name:"String", type:"string"},
+                {name:"Mail", type:"string"}
+             ]
+
+var theme = [
+                {name:"Mail", width:"50", align:"left", editable:true},
+                {name:"Number", width:"50", align:"left", editable:true},
+                {name:"String", width:"50", align:"left",  editable:true}
+            ]
+
