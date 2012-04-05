5d1c69aacaf780711351a7748680866abefa0dc7
Jeremy moyson <moyson.jeremy@gmail.com>
2012-04-05 08:20:55 +0200

    Drop OK + info sur l'element selectionner

---
+++ InterfaceGraphique/js/manage-widget.js
@@ -0,0 +1,23 @@
+function addWidgetDiv(destination)
+{
+/*elem = document.createElement('div');
+elem.setAttribute('class', 'drop-zone widget-div');*/
+var elem = $("<div></div>");
+elem.attr("name", "div");
+elem.attr("class", "drop-zone widget-div");
+$(destination).append(elem);
+
+elem.click(function(){
+						$(".widget-selected").removeClass("widget-selected");
+						$(this).addClass("widget-selected");
+						gui_RefreshForm();
+						}
+			);
+elem.droppable({										greedy: true,
+														activeClass:"ui-state-hover",
+                      									hoverClass:"ui-state-active",
+                                                         drop: function(event, ui) {
+                                                         addToPage(ui.draggable[0], $(this)[0]);
+                                                         }
+                                                         });
+}
